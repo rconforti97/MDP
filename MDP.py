be6cede1b23ab5eeebf50076de810294beb6e62e
Rachel Conforti <50918318+rconforti97@users.noreply.github.com>
2022-04-17 17:10:46 -0400

    comments, reduced redundancy
    sadly not printing the correct outputs

--- MDP.py
+++ MDP.py
@@ -1,4 +1,6 @@
 # Rachel Conforti 
+# All you have to do is run the python file. 
+# Please have numpy package installed 
 
 import numpy as np
 
@@ -64,31 +66,40 @@ P = [[[0.1, 0.1, 0.,  0.,  0.8, 0.,  0.,  0.,  0.,  0.,  0.],
   [0.,  0.,  0.,  0.,  0.,  0.,  0.1, 0.,  0.,  0.8, 0.1]]]
 P = np.array(P)
 
-# think this is where I am going wrong 
-def getExpectedUtility(actions, utilities, P, states, passed_in_position):
+# think this is where I am going wrong or policy print. 
+def getExpectedUtility(utilities, P, states, passed_in_position):
+    # initalizing 
     choosen_action = 0
-    max_expected_utilities = -100000
+    # initalizing our testing point
+    max_expected_utilities = -1
+    # for each action
     for index in range(4):
-
         expected_utilities = 0
+        # for the potential next state of our objects 
         for next_state in states:
+            # getting u prine by getting the utility at the location of the next state
             u_prime = utilities[next_state]
-            next_state = P[index, passed_in_position, next_state]
-            expected_utilities += (next_state * u_prime)
+            # getting the probability from the transition model 
+            prob_of_next_state = P[index, passed_in_position, next_state]
+            # adding the expected utility by multiplying the probability and u prime
+            expected_utilities += (prob_of_next_state * u_prime)
 
+            # This is to see if we hit the max expected utility
+            # to ensure we are chosing the highest utility
+            # if its higher then we add the action to the list
             if expected_utilities > max_expected_utilities:
                 max_expected_utilities = expected_utilities
                 choosen_action = index
-
-    list_of_action = actions[choosen_action]
-
-    return list_of_action
+        # return the list of actions to add to the policy 
+        list_of_actions = actions[choosen_action]
+    
+    return list_of_actions
 
 # This is the value iteration method 
-def valueIteration(P, R_states, discount):
+def valueIteration(P, passed_in_rewards, passed_in_discount):
     delta = 0
     # this is our breaking point of the code
-    threshold = 0.0001 * (1 - discount) / discount
+    threshold = 0.0001 * (1 - passed_in_discount) / passed_in_discount
     # Creating the utility list and setting terminal states
     U = np.zeros(11)
     U[6] = -1
@@ -96,32 +107,32 @@ def valueIteration(P, R_states, discount):
     # Creating u prime
     U_prime = np.zeros(11)
     # creating list of passed in reward states and hard coding terminal states
-    r = np.full(11, R_states) 
+    r = np.full(11, passed_in_rewards) 
     r[6] = -1
     r[10] = 1
 
     while True:
-        for i in range(0, 11):
+        for i in range(11):
             # check if terminal state - if so move on
             if i == 6 or i == 10:
                 continue
             act = np.zeros(4)
             # for each of the 4 possible actions
-            for j in range(0, 4):
+            for j in range(4):
                 # check it against each state
-                for k in range(0, 11):
-                    # the bellman equation  
+                for k in range(11):
+                    # part of the bellman equation  
                     act[j] += (U[k] * P[j][i][k])
 
             # u prime equals reward for this state plus the discount 
-            # plus the highest action from above
-            U_prime[i] = r[i] + discount * max(act)
+            # plus the highest action from above. This is the bellman equation
+            U_prime[i] = r[i] + passed_in_discount * max(act)
             # taking absolute value b/c without it can be below zero automacatically
             delta = abs(U_prime[i] - U[i])
             U[i] = U_prime[i]
         
         # checking for break condition
-        if delta <= threshold:
+        if delta < threshold:
             break
     
     print("Utilities:", U)
@@ -129,23 +140,25 @@ def valueIteration(P, R_states, discount):
 
 # Purpose of this method is to let us know what states are 
 # valid for us to go to. AKA checking if we hit a boundry
-def getValidStates(states, index):
+def getValidStates(objects, state):
     valid_states = []
      
-    # Get the index of each neighbor for a state
-    up = isNextStateValid(states, index.x, index.y+1)
+    # Get the index of neighbor in each direction
+    # and check if its a valid move. Following the 
+    # up, right, down, left cycle. 
+    up = isNextStateValid(objects, state.x, state.y+1)
     if up != -1:
         valid_states.append(up)
 
-    right = isNextStateValid(states, index.x+1, index.y)
+    right = isNextStateValid(objects, state.x+1, state.y)
     if right != -1:
         valid_states.append(right)
 
-    down = isNextStateValid(states, index.x, index.y-1)
+    down = isNextStateValid(objects, state.x, state.y-1)
     if down != -1:
         valid_states.append(down)
 
-    left = isNextStateValid(states, index.x-1, index.y)
+    left = isNextStateValid(objects, state.x-1, state.y)
     if left != -1:
         valid_states.append(left)
 
@@ -160,17 +173,17 @@ def isNextStateValid(states, x_coord, y_coord):
             return index
     return -1
 
-def printPolicy(state_list, utilities, P):
+def printPolicy(utilities, P):
     policy = []
 
-    for index, states in enumerate(state_list):
+    for index, states in enumerate(objects):
         # Checking if we are entering a terminal state
         if index == 6 or index == 10:
             action = 'T'
         # Get the valid states and expect utilities which will choose our actions
         else:
-            valid_states = getValidStates(state_list, states)
-            action = getExpectedUtility(actions, utilities, P, valid_states, index)
+            valid_states = getValidStates(objects, states)
+            action = getExpectedUtility(utilities, P, valid_states, index)
             
         policy.append(action)
 
@@ -178,32 +191,29 @@ def printPolicy(state_list, utilities, P):
     policy.insert(5, '0')
     print(policy[8:12])
     print(policy[4:8])
-    print(policy[0:4])
+    print(policy[:4])
 
+# To clean up my main method a bit, this is printing out the reward
+# and discount, calling my value iteration and print policy method
+def runMDP(P, reward, discount):
+    print("Discount =", discount, " Reward =", reward)
+    value_iteration = valueIteration(P, reward, discount)
+    printPolicy( value_iteration, P)
+    print("\n###################################################################################\n")
+
+# calling the 3 examples from the assignment 
 def main():
-    discount = [0.99]
-    reward = [-0.04]
-    print("Discount =", discount, " Reward =", reward)
-    for discounts, rewards in zip(discount, reward):
-        value_iteration = valueIteration(P, rewards, discounts)
-        printPolicy(objects, value_iteration, P)
-    print("\n###################################################################################\n")
+    discount = 0.99
+    reward = -0.04
+    runMDP(P, reward, discount)
 
-    discount = [0.5]
-    reward = [-0.04]
-    print("Discount =", discount, " Reward =", reward)
-    for discounts, rewards in zip(discount, reward):
-        value_iteration = valueIteration(P, rewards, discounts)
-        printPolicy(objects, value_iteration, P)
+    discount = 0.5
+    reward = -0.04
+    runMDP(P, reward, discount)
 
-    print("\n###################################################################################\n")
-
-    discount = [0.5]
-    reward = [-0.25]
-    print("Discount =", discount, " Reward =", reward)
-    for discounts, rewards in zip(discount, reward):
-        value_iteration = valueIteration(P, rewards, discounts)
-        printPolicy(objects, value_iteration, P)
+    discount = 0.5
+    reward = -0.25
+    runMDP(P, reward, discount)
     
     
 if __name__ == '__main__':
